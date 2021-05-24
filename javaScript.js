6fce44ab25237d2abf618a2950452f76e20608eb
barbelouis <louis.aj.barbe@gmail.com>
2021-05-24 19:09:48 +0200

    Cleaning +css

--- javaScript.js
+++ javaScript.js
@@ -1,80 +1,32 @@
-var $ = document.querySelector.bind(document);
 
- 
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
-
- tag.src = "https://www.youtube.com/iframe_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
- var player, iframe;
- function onYouTubeIframeAPIReady() {
-   player = new YT.Player('player', {
-    playerVars: { 'autoplay': 0, 'controls': 0 },
-     height: '360',
-     width: '640',
-     videoId: 'zUmvvDGAJz4',
-     
-     events: {
-       'onReady': onPlayerReady,
-       'onStateChange': onPlayerStateChange
-     }
-   });
- }
-
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
-   //event.target.playVideo();
-   iframe = $('#player');
-   setupListener();
-   //event.target.playFullscreen(); 
- }
-
- // 5. The API calls this function when the player's state changes.
- //    The function indicates that when playing a video (state=1),
- //    the player should play for six seconds and then stop.
- 
- function onPlayerStateChange(event) {
-   if (event.data == YT.PlayerState.ENDED) {
-
-    go2quote();
+function openFullscreen() {
+  var elem = document.getElementById("myvideo");
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
   }
-
-  if (event.data == YT.PlayerState.PAUSED) {
-
-    player.playVideo();
-  }
-   
- }
- function stopVideo() {
-   player.stopVideo();
- }
-
-
- function setupListener (){
-    $('button').addEventListener('click', playFullscreen);
-    }
-
- function playFullscreen (){
-    player.playVideo();//won't work on mobile
-    
-    var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
-    if (requestFullScreen) {
-      requestFullScreen.bind(iframe)();
-    }
-  }
-  
-
-  function go2quote(){
-    window.location.href='quote.html'  
+  elem.play();
+  hideVideoControls();
 }
+
+
+function hideVideoControls(){
+  var video = document.getElementById('myvideo');
+  video.controls = 0;
+  }
+
 
 function show(){
-   document.getElementById("player").style.visibility = "visible";
+   document.getElementById("myvideo").style.visibility = "visible";
+  // document.getElementById('myVideo').addEventListener('ended',go2quote,false);
+ 
    
 }
-
-
+function startVideo(){
+  
+  show();
+  openFullscreen();
+}
